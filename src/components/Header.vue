19b6976e67d31a64c637af90a54086e6c0cf73ee
Matt0105 <matteo0105@gmail.com>
2022-01-24 12:57:45 +0100

    header completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,35 @@
 <template>
   <header>
       <div class="header-container">
-        <nav>
-          <div class="logo">
-              <img src="../assets/images/light-logo.png" alt="">
-          </div>
-          <div class="navbar__menu-container">
-              <ul class="navbar__menu">
-                  <li><a href="#">Home<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#">Pages<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#">Courses<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#">Features<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#">Blog<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#">Shop<i class="fas fa-chevron-down"></i></a></li>
-                  <li><a href="#"><i class="fas fa-shopping-cart myArea"></i></a></li>
-                  <li><a href="#"><i class="far fa-user-circle myArea"></i></a></li>
-              </ul>
-
-              <div class="input-container">
-                  <input placeholder="Search..." type="text">
-                  <i class="fas fa-search search"></i>
-              </div>
-          </div>
-        </nav>
+        <NavBar />
+        <div class="jumbo">
+            <div class="head-jumbo">
+                <h1>Learn At Your Own Pace!</h1>
+                <p>Get teamed Up with people of the same will.</p>
+                <button>Get started for free</button>
+            </div>
+            
+            <div class="jumbo-imgs">
+                <img class="secondary-first" src="../assets/images/home-6-hero-left-person.png" alt="">
+                <div class="jumbo-poster">
+                    <img src="../assets/images/home-6-hero-poster-final.jpg" alt="">
+                    <img class="yt-logo" src="../assets/images/icon-youtube-play.png" alt="">
+                </div>
+                <img class="secondary-second" src="../assets/images/home-6-hero-right-person-195x300.png" alt="">
+            </div>
+        </div>
       </div>
-      
   </header>
 </template>
 
 <script>
+import NavBar from "./NavBar.vue";
+
 export default {
     name: "Header",
+    components: {
+        NavBar,
+    }
 }
 </script>
 
@@ -39,87 +38,92 @@ export default {
 
     header {
         width: 100%;
-        height: 80px;
+        // height: 780px;
         // background-image: url("../assets/images/jumbotron-background-wave-big.svg");
         background-color: #1f2154;
         color: white;
     }
     .header-container {
-        width: 80%;
+        width: 90%;
         height: 100%;
         margin: 0 auto;
 
-        nav {
-            display: flex;
+        .jumbo {
+            position: relative;
             width: 100%;
-            height: 100%;
-            justify-content: space-between;
-            padding: 1rem 0;
+            text-align: center;
+            // margin-top: 4rem;
+            overflow: hidden;
+            transform: translateY(100px);
 
-            .logo {
-                display: flex;
-                align-items: center;
-                width: 20%;
-                height: 100%;
-                img {
-                    width: 80%;
-                }
+
+            h1 {
+                font-weight: 500;
+                font-size: 3rem;
             }
-            
-            .navbar__menu-container {
+
+            p {
+                color: #f6f3f6;
+                opacity: 0.8;
+            }
+
+            button {
+                margin-top: 1.5rem;
+                padding: 1rem;
+                border: none;
+                border-radius: 5px;
+                color: white;
+                font-weight: 600;
+                background-color: #27ab94;
+            }
+
+            .jumbo-imgs {
+                width: 100%;
                 display: flex;
+                justify-content: center;
                 align-items: center;
-                justify-content: space-between;
 
-                .navbar__menu {
-                display: flex;
-
-                    li {
-                        margin: 0 0.9rem;
-
-                        a {
-                            color: white;
-
-                            i {
-                                font-size: 0.6rem;
-                                margin-left: 0.3rem;
-                                vertical-align: middle;
-                            }
-
-                            i.myArea {
-                                font-size: 1rem;
-                            }
-                        }
-                    }
-
-                }
-
-                .input-container {
+                .jumbo-poster {
+                    position: relative;
+                    bottom: 20px;
                     display: flex;
-                    justify-content: space-between;
+                    justify-content: center;
                     align-items: center;
-                    height: 40px;
+                    width: 50%;
                     background-color: white;
-                    border-radius: 5px;
-                    overflow: hidden;
-                    padding: 0 8px;
+                    border-radius: 20px;
+                    margin-top: 5rem;
+                    box-shadow: 2px 10px 10px rgba(128, 128, 128, 0.288);
 
-                    input {
-                        width: 100%;
-                        height: 100%;
-                        border: none;
-                        outline: none;
+                    img {
+                        width: 95%;
+                        margin: 1rem 0;
                     }
 
-                    .search {
-                        color: #1f9efb;
+                    .yt-logo {
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        width: 100px;
                     }
                 }
+
+                .secondary-first, .secondary-second {
+                    position: absolute;
+                }
+                .secondary-first {
+                    left: 80px;
+                    bottom: 0;
+                }
+                .secondary-second {
+                    right: 14%;
+                    bottom: -140px;
+                }
+
+
             }
-
-            
         }
-
         
     }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,105 @@
+<template>
+    <nav>
+        <div class="logo">
+            <img src="../assets/images/light-logo.png" alt="">
+        </div>
+        <div class="navbar__menu-container">
+            <ul class="navbar__menu">
+                <li><a href="#">Home<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#">Pages<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#">Courses<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#">Features<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#">Blog<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#">Shop<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="#"><i class="fas fa-shopping-cart myArea"></i></a></li>
+                <li><a href="#"><i class="far fa-user-circle myArea"></i></a></li>
+            </ul>
+
+            <div class="input-container">
+                <input placeholder="Search..." type="text">
+                <i class="fas fa-search search"></i>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style lang="scss" scoped>
+
+    nav {
+        display: flex;
+        width: 100%;
+        height: 80px;
+        justify-content: space-between;
+        padding: 1rem 4rem;
+
+        .logo {
+            display: flex;
+            align-items: center;
+            width: 20%;
+            height: 100%;
+            img {
+                width: 80%;
+            }
+        }
+        
+        .navbar__menu-container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .navbar__menu {
+            display: flex;
+
+                li {
+                    margin: 0 0.9rem;
+
+                    a {
+                        color: white;
+
+                        i {
+                            font-size: 0.6rem;
+                            margin-left: 0.3rem;
+                            vertical-align: middle;
+                        }
+
+                        i.myArea {
+                            font-size: 1rem;
+                        }
+                    }
+                }
+
+            }
+
+            .input-container {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                height: 40px;
+                background-color: white;
+                border-radius: 5px;
+                overflow: hidden;
+                padding: 0 8px;
+
+                input {
+                    width: 100%;
+                    height: 100%;
+                    border: none;
+                    outline: none;
+                }
+
+                .search {
+                    color: #1f9efb;
+                }
+            }
+        }
+
+            
+    }
+
+</style>

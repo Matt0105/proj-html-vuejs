f7d9e35f4ed84e13d80c860a857853c8f6fbb705
Matt0105 <matteo0105@gmail.com>
2022-01-24 11:54:47 +0100

    header navbar completed

--- src/App.vue
+++ src/App.vue
@@ -25,11 +25,6 @@ export default {
 @import "./assets/scss/style.scss";
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,30 @@
 <template>
-  <header class="header-container">
-      <nav>
+  <header>
+      <div class="header-container">
+        <nav>
           <div class="logo">
               <img src="../assets/images/light-logo.png" alt="">
           </div>
-      </nav>
+          <div class="navbar__menu-container">
+              <ul class="navbar__menu">
+                  <li><a href="#">Home<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#">Pages<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#">Courses<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#">Features<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#">Blog<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#">Shop<i class="fas fa-chevron-down"></i></a></li>
+                  <li><a href="#"><i class="fas fa-shopping-cart myArea"></i></a></li>
+                  <li><a href="#"><i class="far fa-user-circle myArea"></i></a></li>
+              </ul>
+
+              <div class="input-container">
+                  <input placeholder="Search..." type="text">
+                  <i class="fas fa-search search"></i>
+              </div>
+          </div>
+        </nav>
+      </div>
+      
   </header>
 </template>
 
@@ -15,5 +35,91 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+@import "../assets/scss/partials/_commons.scss";
+
+    header {
+        width: 100%;
+        height: 80px;
+        // background-image: url("../assets/images/jumbotron-background-wave-big.svg");
+        background-color: #1f2154;
+        color: white;
+    }
+    .header-container {
+        width: 80%;
+        height: 100%;
+        margin: 0 auto;
+
+        nav {
+            display: flex;
+            width: 100%;
+            height: 100%;
+            justify-content: space-between;
+            padding: 1rem 0;
+
+            .logo {
+                display: flex;
+                align-items: center;
+                width: 20%;
+                height: 100%;
+                img {
+                    width: 80%;
+                }
+            }
+            
+            .navbar__menu-container {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                .navbar__menu {
+                display: flex;
+
+                    li {
+                        margin: 0 0.9rem;
+
+                        a {
+                            color: white;
+
+                            i {
+                                font-size: 0.6rem;
+                                margin-left: 0.3rem;
+                                vertical-align: middle;
+                            }
+
+                            i.myArea {
+                                font-size: 1rem;
+                            }
+                        }
+                    }
+
+                }
+
+                .input-container {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    height: 40px;
+                    background-color: white;
+                    border-radius: 5px;
+                    overflow: hidden;
+                    padding: 0 8px;
+
+                    input {
+                        width: 100%;
+                        height: 100%;
+                        border: none;
+                        outline: none;
+                    }
+
+                    .search {
+                        color: #1f9efb;
+                    }
+                }
+            }
+
+            
+        }
+
+        
+    }
 </style>

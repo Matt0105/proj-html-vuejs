3e626671f7d8a32f49a90f387a0086a62f0bd6f0
Matt0105 <matteo0105@gmail.com>
2022-01-24 10:50:30 +0100

    scaffholding

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header-container">
+      <nav>
+          <div class="logo">
+              <img src="../assets/images/light-logo.png" alt="">
+          </div>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
